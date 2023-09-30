29795efd38049c4161d47fec508ec8073f11382f
s0a1e2 <oops2198@gmail.com>
2023-09-30 11:05:58 +0330

    base of user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Column, ObjectId, ObjectIdColumn } from 'typeorm';
+
+export class User {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
