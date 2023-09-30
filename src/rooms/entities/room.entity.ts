679ead7a61854fa69d24a68a4c670ca6dd8fd908
s0a1e2 <oops2198@gmail.com>
2023-09-30 11:13:41 +0330

    add base of room entity

--- src/rooms/entities/room.entity.ts
+++ src/rooms/entities/room.entity.ts
@@ -1,1 +1,12 @@
-export class RoomEntity {}
+import { Column, ObjectId, ObjectIdColumn } from 'typeorm';
+
+export class RoomEntity {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+}
