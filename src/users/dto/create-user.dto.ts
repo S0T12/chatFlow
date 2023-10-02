10b195add41d7c2eed2d047b09d88c2e41b5f8a0
s0a1e2 <oops2198@gmail.com>
2023-10-02 12:15:14 +0330

    add create user DTO and update users.http

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  lastName: string;
+
+  @IsOptional()
+  dateOfBirth: Date;
+
+  @IsUrl()
+  profilePictureUrl: string;
+}
