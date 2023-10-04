af3be67bba5167984f5f8228da2060dffbddd6dd
s0a1e2 <oops2198@gmail.com>
2023-10-05 01:51:07 +0330

    add globally configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
