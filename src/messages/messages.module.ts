08803c2be7cf6640235dc99d691edd9235198193
s0a1e2 <oops2198@gmail.com>
2023-10-05 16:08:49 +0330

    add env sample

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesGateway } from './messages.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { MessageEntity } from './entities/message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessagesModule])],
+  imports: [
+    TypeOrmModule.forFeature([MessageEntity]),
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        config: {
+          host: 'localhost',
+          port: 6379,
+        },
+      }),
+    }),
+  ],
   providers: [MessagesGateway, MessagesService],
 })
 export class MessagesModule {}
