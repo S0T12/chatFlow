6cd17199840b0f7662a75b34c0db3b9f7f2f0ea0
s0a1e2 <oops2198@gmail.com>
2023-10-13 12:25:39 +0330

    update create method

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -2,24 +2,39 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { ObjectId } from 'typeorm';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway(8081)
 export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly messagesService: MessagesService) {}
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    console.log(
+      'Received createMessage event with createMeesageDto:',
+      createMessageDto,
+    );
+    const message = await this.messagesService.create(createMessageDto);
+
+    if (message) {
+      this.server.emit('createMessage', message);
+    } else {
+      this.server.emit('createMessage', 'Something is wrong');
+    }
   }
 
   @SubscribeMessage('findAllMessages')
   findAll() {
-    return this.messagesService.findAll();
+    this.server.emit('findAllMessages', this.messagesService.findAll());
   }
 
   @SubscribeMessage('findOneMessage')
