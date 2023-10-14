cd0d8c696645f9dba3cd147b00edc683edf73716
s0a1e2 <oops2198@gmail.com>
2023-10-14 14:09:30 +0330

    remove redis for now

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,25 +2,13 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesGateway } from './messages.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { MessageEntity } from './entities/message.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { RoomsModule } from '../rooms/rooms.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MessageEntity]),
-    RedisModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        config: {
-          host: configService.get('REDIS_HOST'),
-          port: configService.get('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
     UsersModule,
     RoomsModule,
   ],
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -5,36 +5,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MessageEntity } from './entities/message.entity';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
-import Redis from 'ioredis';
 import { UsersService } from '../users/users.service';
 import { RoomsService } from '../rooms/rooms.service';
 
 @Injectable()
 export class MessagesService {
-  private redisClient: Redis;
-
   constructor(
     private roomsService: RoomsService,
     private usersService: UsersService,
     @InjectRepository(MessageEntity)
     private messageRepo: Repository<MessageEntity>,
-  ) {
-    this.redisClient = new Redis();
-  }
+  ) {}
 
   async create(createMessageDto: CreateMessageDto) {
     try {
-      console.log(
-        'Received request to create message with DTO:',
-        createMessageDto,
-      );
       const { content, senderUsername, roomLink } = createMessageDto;
 
       const sender = await this.usersService.findOne(senderUsername);
-      console.log('Retrieved sender:', sender);
 
       const room = await this.roomsService.findOne(roomLink);
-      console.log('Retrieved room:', room);
 
       if (!sender) throw new Error('Sender not found');
       if (!room) throw new Error('Room not found');
@@ -44,13 +33,12 @@ export class MessagesService {
         sender,
         room,
       });
-      console.log('Created message object:', message);
 
       const savedMessage = await this.messageRepo.save(message);
-      console.log('Saved message object:', savedMessage);
       return savedMessage;
     } catch (error) {
       console.log(error);
+      throw new Error('something wrong!');
     }
   }
 
@@ -65,15 +53,11 @@ export class MessagesService {
   async update(id: number, updateMessageDto: UpdateMessageDto) {
     await this.messageRepo.update(id, updateMessageDto);
 
-    await this.redisClient.del('messages');
-
     return { updated: true };
   }
 
   async remove(id: number) {
     await this.messageRepo.delete(id);
-
-    await this.redisClient.del('messages');
 
     return { deleted: true };
   }
