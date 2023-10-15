fe873d543d38868fc0e0d2f7b1eaafe68598256c
s0a1e2 <oops2198@gmail.com>
2023-10-15 21:15:07 +0330

    config Monoose in modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,29 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { MessagesModule } from './messages/messages.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './users/entities/user.entity';
-import { MessageEntity } from './messages/entities/message.entity';
-import { RoomEntity } from './rooms/entities/room.entity';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mongodb',
-        url: configService.get('MONGODB_URI'),
-        database: configService.get('MONGODB_DATABASE'),
-        autoLoadEntities: true,
-        logging: true,
-        entities: [UserEntity, RoomEntity, MessageEntity],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/chat-flow'),
     UsersModule,
     RoomsModule,
     MessagesModule,
