09e52bf8ca1097338022a184d4da593f6c22278b
s0a1e2 <oops2198@gmail.com>
2023-10-21 03:23:01 +0330

    add redis

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -6,6 +6,7 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { UsersService } from '../users/users.service';
 import { RoomsService } from '../rooms/rooms.service';
+import { RedisService } from '@liaoliaots/nestjs-redis';
 
 @Injectable()
 export class MessagesService {
@@ -13,6 +14,7 @@ export class MessagesService {
     @InjectModel('messages') private readonly messageModel: Model<Message>,
     private readonly roomsService: RoomsService,
     private readonly usersService: UsersService,
+    private readonly redisService: RedisService,
   ) {}
 
   async create(createMessageDto: CreateMessageDto) {
@@ -35,18 +37,48 @@ export class MessagesService {
       sender,
       room,
     });
-    return await createdMessage.save();
+
+    const savedMessage = await createdMessage.save();
+
+    // Update Redis cache with the new message.
+    await this.redisService
+      .getClient()
+      .set(`message:${savedMessage._id}`, JSON.stringify(savedMessage));
+
+    return savedMessage;
   }
 
   async findAll() {
-    return await this.messageModel.find().exec();
+    const cachedMessages = await this.redisService.getClient().get('messages');
+    if (cachedMessages) {
+      return JSON.parse(cachedMessages);
+    }
+
+    const messages = await this.messageModel.find().exec();
+    await this.redisService
+      .getClient()
+      .set('messages', JSON.stringify(messages));
+
+    return messages;
   }
 
   async findOne(id: string) {
+    const cachedMessage = await this.redisService
+      .getClient()
+      .get(`message:${id}`);
+    if (cachedMessage) {
+      return JSON.parse(cachedMessage);
+    }
+
     const message = await this.messageModel.findOne({ _id: id }).exec();
     if (!message) {
       throw new NotFoundException(`Message with id ${id} not found`);
     }
+
+    await this.redisService
+      .getClient()
+      .set(`message:${id}`, JSON.stringify(message));
+
     return message;
   }
 
@@ -57,6 +89,12 @@ export class MessagesService {
     if (!updatedMessage) {
       throw new NotFoundException(`Message with id ${id} not found`);
     }
+
+    // Update Redis cache with the updated message.
+    await this.redisService
+      .getClient()
+      .set(`message:${id}`, JSON.stringify(updatedMessage));
+
     return updatedMessage;
   }
 
@@ -65,6 +103,10 @@ export class MessagesService {
     if (!deletedMessage) {
       throw new NotFoundException(`Message with id ${id} not found`);
     }
+
+    // Remove the message from Redis cache
+    await this.redisService.getClient().del(`message:${id}`);
+
     return { deleted: true };
   }
 }
